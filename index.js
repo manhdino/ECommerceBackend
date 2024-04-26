36bb1b50da8012ada17a9bb60a690e24226316fa
manhdino <manhnguyen1238@gmail.com>
2024-04-26 18:28:51 +0700

    fix init project

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/connectDB");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-connectDB();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
