03391053fa830e8818e82b5b7331d19366ded4a3
IMnhvjp <levanminh19102003@gmail.com>
2024-05-09 23:49:54 +0700

    fix

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,13 +4,13 @@ const { getAllUser, testAccessDenied } = require("../controllers/user_controller
 const passport = require('passport');
 const router = require('express').Router();
 
-router.use(passport.initialize());
-router.use(passport.session());
+// router.use(passport.initialize());
+// router.use(passport.session());
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8000/api/user/auth/google/callback"
+    callbackURL: "http://localhost:3000/api/user/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
     console.log(">>>access token:", accessToken);
@@ -24,10 +24,10 @@ router.get("/getAllUser", getAllUser);
 
 //dang nhap voi google
 router.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile'] }));
+  passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 router.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/accessDenied' }),
+  passport.authenticate('google', { session: false, failureRedirect: '/accessDenied' }),
   function(req, res) {
     // Successful authentication, redirect home.
     res.redirect('/api/user/getAllUser');
