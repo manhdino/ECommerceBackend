c5af100f3b4708f069c8dd95691c7e5402d3ba31
manhdino <manhnguyen1238@gmail.com>
2024-05-10 22:57:25 +0700

    fix deploy swagger railway

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000/api",
+        url: "ecommercebackend-production-ad86.up.railway.app/api",
       },
     ],
   },
