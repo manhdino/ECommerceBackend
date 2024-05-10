19c7c6e9be86e6c876d505cbfa2090d2f79e2d9c
manhdino <manhnguyen1238@gmail.com>
2024-05-10 21:47:53 +0700

    add swagger test API

--- index.js
+++ index.js
@@ -3,17 +3,45 @@ const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const routes = require("./routes");
 const PORT = process.env.PORT || 3000;
 const app = express();
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-const userRouter = require("./routes/auth.route.js");
-app.use("/api/user", userRouter);
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Open API Coding Blog ",
+      version: "1.0.11",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./swagger/*.yaml"],
+};
 
+const openapiSpecification = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+
+routes(app);
+
+app.use((req, res) => {
+  return res.status(500).send({
+    success: false,
+    status: 500,
+    message: "Internal Server Error",
+  });
+});
 app.listen(PORT, () => {
   console.log(`Server is running  at http://localhost:${PORT}`);
 });
