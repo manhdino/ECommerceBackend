3e1bf37219edbb5326a4e351b386df8d1e10177a
IMnhvjp <levanminh19102003@gmail.com>
2024-05-11 23:46:58 +0700

    add forgot password, change password

--- services/auth.services.js
+++ services/auth.services.js
@@ -1,99 +1,235 @@
-const model = require("../database/models");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const rs = require("../helpers/error");
+const db = require('../database/models');
+const {sendMail, emailContent} = require('./mail.services');
+const { where } = require('sequelize');
+const User = db.User;
+
+// const hashPassword = async (password) => {
+//     console.log(password);
+//     return bcrypt.hash(password, 10);
+// }
+const generateToken = async (userId, role, time) => {
+    try{
+        const payload = { userId: userId, role: role};
+        const token =  jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: time });
+        return {
+            token
+        }
+    }
+    catch(err) {
+        return {
+            error: err
+        }
+    }
+}
+
+const verifyToken = (token) => {
+    try {
+        jwt.verify(
+            token,
+            process.env.JWT_SECRET,
+            (err, decoded) => {
+                // if (err) return ({
+                //     success: false,
+                //     message: 'Invalid token'
+                // })
+                if (err != null) throw err;
+                return decoded.payload;
+            }
+        )
+    } catch (err) {
+        throw err;
+        // console.log("[JWT] Error getting JWT token:", e.message)
+    }
+}
+
+const signIn = async (data) => {
+    try {
+        const { email, password } = data;
+        const checkUser = await model.User.findOne({
+          where: {
+            email: email,
+          },
+        });
+        if (!checkUser) {
+          return {
+            error: "Email not found",
+          };
+        }
+        if (checkUser.googleId) {
+          return {
+            error: "This email is registered as a Google account."
+          }
+        }
+        const isPasswordValid = await bcrypt.compare(
+          password,
+          checkUser.password
+        );
+  
+        if (!isPasswordValid) {
+          return {
+            error: "Invalid password",
+          };
+        }
+        let refreshToken = checkUser.refreshToken;
+        const checkVerify = authService.verifyToken(refreshToken);
+        if (refreshToken === null || checkVerify === null) {
+            refreshToken = authService.generateToken(checkUser.id, checkUser.role, "7d");
+            await User.update({
+                refreshToken: refreshToken
+            }, {where: {id: checkUser.id}})
+        }
+        const access_token = jwt.sign(
+          { userId: checkUser.id, role: checkUser.role },
+          process.env.JWT_SECRET_KEY,
+          {
+            expiresIn: "15m",
+          }
+        );
+        return {
+          data: {
+            user: {
+              id: checkUser.id,
+              username: checkUser.username,
+              email: checkUser.email,
+              fullname: checkUser.fullname,
+              role: checkUser.role,
+              phone: checkUser.phone,
+              address: checkUser.address,
+              created_at: checkUser.created_at,
+              updated_at: checkUser.updated_at,
+            },
+            refreshToken: refreshToken,
+            access_token: access_token,
+          },
+        };
+      } catch (error) {
+        return {
+          data: error.message,
+        };
+      }
+}
+
+const signUp = async (data) => {
+    try {
+        const { email, password } = data;
+        const checkUser = await model.User.findOne({
+          where: {
+            email: email,
+          },
+        });
+        if (checkUser) {
+          return {
+            error: "Email is already in used",
+          };
+        }
+        const newUser = await model.User.create({
+          email: email,
+          password: password,
+          username: email,
+          created_at: new Date(),
+          updated_at: new Date(),
+        });
+        return {
+          data: {
+            id: newUser.id,
+            username: newUser.username,
+            email: newUser.email,
+            fullname: newUser.fullname,
+            role: newUser.role,
+            phone: newUser.phone,
+            address: newUser.address,
+            created_at: newUser.created_at,
+            updated_at: newUser.updated_at,
+          },
+        };
+      } catch (error) {
+        return {
+          data: error.message,
+        };
+      }
+}
+
+const refreshToken = async (refreshToken) => {
+    try {
+        const checkVerify = await verifyToken(refreshToken)
+        if (checkVerify) {
+            const response = await User.findOne({id: checkVerify.id, refreshToken: refreshToken})
+            return ({
+                accessToken: response ? jwt.sign(checkVerify, process.env.JWT_SECRET, { expiresIn: '15m' }) : null
+            })
+        }
+    } catch (err) {
+        return ({
+            error: err.message,
+        });
+    }
+}
+
+const changePassword = async (email, newPassword) => {
+  try {
+    const user = await model.User.findOne({
+      where: {
+        email: email
+      }
+    })
+    if (!user) {
+      return {
+        error: "Email not found"
+      };
+    }
+    if (user.googleId) {
+      return {
+        error: "This email is registered as a Google account."
+      }
+    }
+    user.password = newPassword;
+    user.passwordCode = null;
+    await user.save();
+    return {
+      message: "password updated"
+    }
+  }
+  catch (err) {
+    return {
+      error: err
+    }
+  }
+}
+
+const forgotPassword = async (email) => {
+  try {
+    const user = await User.findOne({
+      where: email
+    })
+    if (!user) {
+      return {
+        error: "Email not found"
+      }
+    }
+    if (user.googleId) {
+      return {
+        error: "This email is registered as a Google account."
+      }
+    }
+    const host = req.header('host')
+    const token = jwt.sign({userId : user.id, role: user.role}, process.env.JWT_SECRET_KEY, {expiresIn: '5m'});
+    const resetLink = `${req.protocol}://${host}/reset?token=${token}&email=${email}`;
+    const html = emailContent(user.fullname, host);
+    sendMail(email, html)
+  }
+  catch(error) {
+
+  }
+}
 
 module.exports = {
-  signIn: async (data) => {
-    try {
-      const { email, password } = data;
-      const checkUser = await model.User.findOne({
-        where: {
-          email: email,
-        },
-      });
-      if (!checkUser) {
-        return {
-          error: "Email not found",
-        };
-      }
-      const isPasswordValid = await bcrypt.compare(
-        password,
-        checkUser.password
-      );
-
-      if (!isPasswordValid) {
-        return {
-          error: "Invalid password",
-        };
-      }
-
-      const access_token = jwt.sign(
-        { userId: checkUser.id, role: checkUser.role },
-        process.env.JWT_SECRET_KEY,
-        {
-          expiresIn: "5h",
-        }
-      );
-
-      return {
-        data: {
-          user: {
-            id: checkUser.id,
-            username: checkUser.username,
-            email: checkUser.email,
-            fullname: checkUser.fullname,
-            role: checkUser.role,
-            phone: checkUser.phone,
-            address: checkUser.address,
-            created_at: checkUser.created_at,
-            updated_at: checkUser.updated_at,
-          },
-          access_token: access_token,
-        },
-      };
-    } catch (error) {
-      return {
-        data: error.message,
-      };
-    }
-  },
-  signUp: async (data) => {
-    try {
-      const { email, password } = data;
-      const checkUser = await model.User.findOne({
-        where: {
-          email: email,
-        },
-      });
-      if (checkUser) {
-        return {
-          error: "Email is already in used",
-        };
-      }
-      const newUser = await model.User.create({
-        email: email,
-        password: password,
-        username: email,
-        created_at: new Date(),
-        updated_at: new Date(),
-      });
-      return {
-        data: {
-          id: newUser.id,
-          username: newUser.username,
-          email: newUser.email,
-          fullname: newUser.fullname,
-          role: newUser.role,
-          phone: newUser.phone,
-          address: newUser.address,
-          created_at: newUser.created_at,
-          updated_at: newUser.updated_at,
-        },
-      };
-    } catch (error) {
-      return {
-        data: error.message,
-      };
-    }
-  },
-};
+    generateToken,
+    verifyToken,
+    refreshToken,
+    signIn,
+    signUp,
+    changePassword
+}
---
+++ services/mail.services.js
@@ -0,0 +1,47 @@
+const nodemailer = require('nodemailer')
+const asyncHandler = require('express-async-handler')
+const sendMail = async (email, html) => {
+    try{
+        let transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false,
+            auth: {
+                user: process.env.EMAIL_NAME,
+                pass: process.env.EMAIL_APP_PASSWORD,
+            },
+        });
+        let info = await transporter.sendMail({
+            from: '"vanminh" <no-relply@vanminh.com>',
+            to: email,
+            subject: "Forgot password",
+            html: html,
+        });
+        return info
+    }
+    catch (err) {
+        return {
+            error: err
+        }
+    }
+}
+
+const emailContent = (fullname, host) => {
+    return `
+        Hello ${fullname},
+
+        You recently requested to reset the password for your ${host} account.
+        Click the link below to proceed.
+
+        <a href="${resetLink}">Reset Password</a>
+
+        If you did not request a password reset, please ignore this email or reply to let us know. This password reset link is only valid for the next 5 minutes.
+
+        Thanks,
+    `;
+}
+
+module.exports = {
+    sendMail,
+    emailContent
+}
