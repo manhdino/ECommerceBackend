c788ac5c23cb5b56bb352679aa530a6d723f13e6
IMnhvjp <levanminh19102003@gmail.com>
2024-05-12 20:16:19 +0700

    fix user route

--- routes/user.route.js
+++ routes/user.route.js
@@ -8,8 +8,8 @@ const { auth, admin } = require("../middleware/auth");
 // router.put("/update/:userId", [auth, admin], userController.update);
 // router.delete("/delete/:userId", [auth, admin], userController.destroy);
 
-router.get("/", admin, userController.index);
-router.get("/:userId", auth, userController.show);
-router.put("/update/:userId", auth, userController.update);
-router.delete("/delete/:userId", admin, userController.destroy);
+// router.get("/", admin, userController.index);
+router.get("/infor", auth, userController.show);
+router.put("/update", auth, userController.update);
+router.delete("/delete", auth, userController.destroy);
 module.exports = router;
