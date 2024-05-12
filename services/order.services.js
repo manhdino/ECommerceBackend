fca5e8a30bf458a8e4d6b47cf58b0810991cdb01
manhdino <manhnguyen1238@gmail.com>
2024-05-12 21:03:43 +0700

    add order api

--- services/order.services.js
+++ services/order.services.js
@@ -1,17 +1,39 @@
+const { sequelize } = require("../database/models");
 const model = require("../database/models");
 
 module.exports = {
-  index: async () => {
+  index: async (data) => {
     try {
-      const response = await model.Order.findAll({
-        attributes: { exclude: ["user_id"] },
-        include: [
-          {
-            model: model.User,
-            as: "ordered_by",
+      const { userId, role } = data;
+      let response = null;
+      if (role == "admin") {
+        response = await model.Order.findAll({
+          attributes: { exclude: ["user_id"] },
+          include: [
+            {
+              model: model.User,
+              as: "ordered_by",
+              attributes: { exclude: ["created_at", "updated_at", "password"] },
+            },
+          ],
+        });
+      }
+      if (role == "user") {
+        response = await model.Order.findAll({
+          where: {
+            user_id: userId,
           },
-        ],
-      });
+          attributes: { exclude: ["user_id"] },
+          include: [
+            {
+              model: model.User,
+              as: "ordered_by",
+              attributes: { exclude: ["created_at", "updated_at", "password"] },
+            },
+          ],
+        });
+      }
+
       if (response) {
         return {
           data: response,
@@ -26,9 +48,30 @@ module.exports = {
       };
     }
   },
-  show: async (orderId) => {
+  show: async (data) => {
     try {
-      const response = await model.Order.findByPk(orderId);
+      const { orderId } = data;
+      const checkOrder = await model.Order.findByPk(orderId);
+      if (!checkOrder) {
+        return {
+          error: "Order not found",
+        };
+      }
+      const response = await model.OrderDetails.findAll({
+        where: {
+          order_id: orderId,
+        },
+        attributes: { exclude: ["order_id"] },
+        include: [
+          {
+            model: model.Product,
+            as: "product",
+            attributes: {
+              exclude: ["created_at", "updated_at", "id"],
+            },
+          },
+        ],
+      });
       if (!response) {
         return {
           error: "Order not found",
@@ -45,28 +88,88 @@ module.exports = {
   },
   create: async (data) => {
     try {
-      //   const response = await model.Product.create({});
-      const response = 1;
-      return response == 1
-        ? "Product created sucessfully"
-        : "Failed to create product";
+      const { paymentMethod, amount, ...userInfo } = data;
+
+      const result = await sequelize.transaction(async (t) => {
+        const createdOrder = await model.Order.create(
+          {
+            user_id: userInfo.userId,
+            payment_method: paymentMethod,
+            amount: amount,
+            status: "created",
+            created_at: new Date(),
+            updated_at: new Date(),
+          },
+          { transaction: t }
+        );
+
+        if (createdOrder) {
+          const products = await model.Cart.findAll(
+            {
+              where: {
+                user_id: userInfo.userId,
+              },
+            },
+            { transaction: t }
+          );
+
+          if (products) {
+            const orderDetailsPromises = products.map(async (product) => {
+              await model.OrderDetails.create(
+                {
+                  product_id: product.product_id,
+                  order_id: createdOrder.id,
+                  price: product.price,
+                  quantity: product.quantity,
+                  created_at: new Date(),
+                  updated_at: new Date(),
+                },
+                { transaction: t }
+              );
+            });
+
+            await Promise.all(orderDetailsPromises);
+          }
+        }
+
+        return createdOrder;
+      });
+
+      if (result) {
+        return {
+          data: result,
+        };
+      } else {
+        return {
+          error: "Failed to create new order",
+        };
+      }
     } catch (error) {
       return {
         error: error.message,
       };
     }
   },
-
-  update: async (orderId) => {
+  update: async (data) => {
     try {
+      const { orderId, status } = data;
       const checkOrder = await model.Order.findByPk(orderId);
       if (!checkOrder) {
         return {
           error: "Order not found",
         };
       }
-      //   const response = await db.Category.update();
-      const response = 1;
+      const response = await model.Order.update(
+        {
+          status: status,
+        },
+        {
+          where: {
+            id: orderId,
+          },
+        }
+      );
+
       return {
         data:
           response == 1
@@ -79,17 +182,19 @@ module.exports = {
       };
     }
   },
-  destroy: async (orderId) => {
+  destroy: async (data) => {
     try {
+      const { orderId, ...userInfo } = data;
       const checkOrder = await model.Order.findByPk(orderId);
       if (!checkOrder) {
         return {
           error: "Order not found or has been deleted",
         };
       }
-      const response = await db.Order.destroy({
+      const response = await model.Order.destroy({
         where: {
-          id: productId,
+          id: orderId,
+          user_id: userInfo.userId,
         },
       });
       return {
