93ba93c6c0b4f97f30ee6dbd93437b7ec97a67d0
manhdino <manhnguyen1238@gmail.com>
2024-05-13 17:22:39 +0700

    fix create product

--- routes/product.route.js
+++ routes/product.route.js
@@ -6,7 +6,7 @@ const { auth, admin } = require("../middleware/auth");
 router.get("/", productController.index);
 router.get("/:productId", productController.show);
 router.get("/list/:categoryId", productController.list);
-router.post("create", [auth, admin], productController.create);
+router.post("/create", [auth, admin], productController.create);
 router.put("/update/:productId", [auth, admin], productController.update);
 router.delete("/delete/:productId", [auth, admin], productController.destroy);
 module.exports = router;
